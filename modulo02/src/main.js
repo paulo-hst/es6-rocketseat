cff7e22fd479aea67451a2974906992d6a7fd114
Paulo Teixeira <hst.paulo@gmail.com>
2020-12-06 01:20:17 -0300

    Módulo 02

---
+++ modulo02/src/main.js
@@ -0,0 +1,19 @@
+/*
+
+// import { soma, sub } from './funcoes.js';
+// import soma, { sub as Subtracao } from './funcoes.js'; // as: 'apelido' / sem {} = export default
+
+import funcaoSoma from './soma';
+console.log(funcaoSoma(5,2));
+
+// console.log(soma(1,2));
+// console.log(Subtracao(1,2));
+
+
+import * as funcoes from './funcoes';
+console.log(funcoes.mult(5,2));
+console.log(funcoes.sub(15,2));
+
+*/
+
+alert('ola asdasdasd');
