6e127d27a2538f3e91a73409370c2335e23abea3
Paulo Teixeira <hst.paulo@gmail.com>
2020-12-06 21:50:22 -0300

    exercicios modulo 02

---
+++ modulo02/exercicios/functions.js
@@ -0,0 +1,9 @@
+/* Exercícios: Módulo 02 */ 
+
+export const idade = 27;
+
+export default class Usuario{
+    static info(){
+        console.log('Apenas Teste');
+    }
+}
---
+++ modulo02/exercicios/main.js
@@ -0,0 +1,7 @@
+
+import ClasseUsuario from './functions';
+import { idade as IdadeUsuario } from './functions';
+
+ClasseUsuario.info();
+console.log(`Idade do usuário: ${IdadeUsuario}`);
+
